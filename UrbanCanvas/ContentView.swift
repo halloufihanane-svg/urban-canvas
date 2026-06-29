36e51b134370d40ee335537840597330bb70dc17
halloufihanane-svg <halloufi.hanane@gmail.com>
2026-06-29 02:14:11 +0200

    Add button liste and carte

--- UrbanCanvas/ContentView.swift
+++ UrbanCanvas/ContentView.swift
@@ -11,7 +11,7 @@ import MapKit
 struct ContentView: View {
     @State private var filterStreetArt = "Tous"
     @State private var showFilterSheet = false
-    @State private var displayMode = "Liste"
+    @State private var displayMode = "liste"
     @State private var cameraPosition: MapCameraPosition = .region(MKCoordinateRegion(center: (CLLocationCoordinate2D(latitude: 46.6, longitude: 2.5)), span: MKCoordinateSpan(latitudeDelta: 8, longitudeDelta: 8))) //center : coordonnées géographiques du centre de la France
     @State private var selectedStreetArt: StreetArtStructure?
     @State private var currentDetent: PresentationDetent = .height(320)
@@ -37,106 +37,126 @@ struct ContentView: View {
                     .ignoresSafeArea()
                 
                 VStack(alignment: .leading) {
-                    Text("Liste des Street arts")
-                        .font(.largeTitle)
-                        .bold()
-                        .padding(.top)
-                        .padding(.horizontal, 20) // Marge appliqué sur Figma dans le dev Mode
-                    HStack {
-                        Button("Liste") {
-                            displayMode = "Liste"
+                    if displayMode == "liste" { // Le titre ne s'affiche que si on est en mode liste
+                        Text("Liste des Street arts")
+                            .font(.largeTitle)
+                            .bold()
+                            .padding(.top)
+                            .padding(.horizontal, 20) // Marge appliqué sur Figma dans le dev Mode
+                    }
+                    HStack(spacing: 0) {
+                        Button {
+                            displayMode = "liste"
+                        } label: {
+                            Text("liste")
+                                .fontWeight(.semibold)
+                                .frame(maxWidth: .infinity)
+                                .padding(.vertical, 10)
+                                .background(displayMode == "liste" ? Color.orange : Color.white)
+                                .foregroundColor(.black)
                         }
-                        Button("Carte") {
-                            displayMode = "Carte"
+                        Button {
+                            displayMode = "carte"
+                        } label: {
+                            Text("carte")
+                                .fontWeight(.semibold)
+                                .frame(maxWidth: .infinity)
+                                .padding(.vertical, 10)
+                                .background(displayMode == "carte" ? Color.orange : Color.white)
+                                .foregroundColor(.black)
                         }
                     }
-                    .padding(.horizontal, 20)
-                    
-                    if displayMode == "Liste" {
-                        List(filteredStreetArts, id: \.self) { streetArt  in // id: \.self car notre structure est Hashable. De plus on remplace streetArtsData de la V1 par filteredStreetArts pour notre filtre dans la V2
-                            
-                            NavigationLink(destination: StreetArtDetailView(streetArt: streetArt)) {
+                        .frame(height: 38)
+                        .background(Color.white)
+                        .clipShape(Capsule())
+                        .padding(.horizontal, 20)
+                        
+                        if displayMode == "liste" {
+                            List(filteredStreetArts, id: \.self) { streetArt  in // id: \.self car notre structure est Hashable. De plus on remplace streetArtsData de la V1 par filteredStreetArts pour notre filtre dans la V2
                                 
-                                HStack (spacing: 11) { // marge de 11 sur Figma entre l'image Circle et le text (le titre de la StreetArt)
-                                    Image(streetArt.imageName)
-                                        .resizable()
-                                        .frame(width: 60, height: 60)
-                                        .scaledToFill()
-                                        .frame(width: 60, height: 60)
-                                        .clipped()
-                                        .cornerRadius(60)
+                                NavigationLink(destination: StreetArtDetailView(streetArt: streetArt)) {
                                     
-                                    VStack (alignment: .leading, spacing: 8) { // forcer l'alignement à droite et espace de 8 sur Figma
-                                        Text(streetArt.title)
-                                            .font(.headline)
+                                    HStack (spacing: 11) { // marge de 11 sur Figma entre l'image Circle et le text (le titre de la StreetArt)
+                                        Image(streetArt.imageName)
+                                            .resizable()
+                                            .frame(width: 60, height: 60)
+                                            .scaledToFill()
+                                            .frame(width: 60, height: 60)
+                                            .clipped()
+                                            .cornerRadius(60)
                                         
-                                        
-                                        HStack (spacing: 4) {
-                                            Image(systemName: "mappin.circle.fill")
-                                                .resizable()
-                                                .frame(width: 12, height: 12)
-                                                .foregroundStyle(.gray)
-                                            Text(streetArt.city)
+                                        VStack (alignment: .leading, spacing: 8) { // forcer l'alignement à droite et espace de 8 sur Figma
+                                            Text(streetArt.title)
                                                 .font(.headline)
-                                                .foregroundStyle(.gray)
+                                            
+                                            
+                                            HStack (spacing: 4) {
+                                                Image(systemName: "mappin.circle.fill")
+                                                    .resizable()
+                                                    .frame(width: 12, height: 12)
+                                                    .foregroundStyle(.gray)
+                                                Text(streetArt.city)
+                                                    .font(.headline)
+                                                    .foregroundStyle(.gray)
+                                            }
                                         }
                                     }
                                 }
                             }
-                        }
-                        .listStyle(.plain)
-                        .scrollContentBackground(.hidden)
-                        //.background(Color.backgroundGray)
-                        .padding(.horizontal, 20)
-                    } else {
-                        Map(position: $cameraPosition, selection: $selectedStreetArt) {
-                            ForEach(filteredStreetArts, id: \.self) { streetArt in
-                                Marker(streetArt.title, coordinate: CLLocationCoordinate2D(latitude: streetArt.latitude, longitude: streetArt.longitude))
-                                    .tint(.mainOrange)
-                                    .tag(streetArt)
+                            .listStyle(.plain)
+                            .scrollContentBackground(.hidden)
+                            //.background(Color.backgroundGray)
+                            .padding(.horizontal, 20)
+                        } else {
+                            Map(position: $cameraPosition, selection: $selectedStreetArt) {
+                                ForEach(filteredStreetArts, id: \.self) { streetArt in
+                                    Marker(streetArt.title, coordinate: CLLocationCoordinate2D(latitude: streetArt.latitude, longitude: streetArt.longitude))
+                                        .tint(.mainOrange)
+                                        .tag(streetArt)
+                                }
+                            }
+                            .sheet(item: $selectedStreetArt) { streetArt in
+                                StreetArtDetailView(streetArt: streetArt, onClose: {
+                                    selectedStreetArt = nil
+                                }, onExpand: {
+                                    currentDetent = .large
+                                })
+                                .presentationDetents([.height(320), .large])
+                                .presentationDragIndicator(.visible)
+                            }
+                            
+                        } //fin VStack
+                        if showFilterSheet {
+                            ZStack(alignment: .topTrailing) {
+                                // Le fond noir translucide pour assombrir la liste
+                                Color.black.opacity(0.3)
+                                    .ignoresSafeArea()
+                                    .onTapGesture {
+                                        // Ferme le menu si on clique en dehors de la carte
+                                        showFilterSheet = false
+                                    }
+                                
+                                FilterMenuView(selectedFilter: $filterStreetArt, isPresented: $showFilterSheet)
+                                
+                                    .padding(.top, 2)
+                                    .padding(.trailing, 16)
+                                    .transition(.scale.combined(with: .opacity)) // Animation zoom + fondu
                             }
                         }
-                        .sheet(item: $selectedStreetArt) { streetArt in
-                            StreetArtDetailView(streetArt: streetArt, onClose: {
-                                selectedStreetArt = nil
-                            }, onExpand: {
-                                currentDetent = .large
-                            })
-                            .presentationDetents([.height(320), .large])
-                            .presentationDragIndicator(.visible)
-                        }
-                               
-                    } //fin VStack
-                    if showFilterSheet {
-                        ZStack(alignment: .topTrailing) {
-                            // Le fond noir translucide pour assombrir la liste
-                            Color.black.opacity(0.3)
-                                .ignoresSafeArea()
-                                .onTapGesture {
-                                    // Ferme le menu si on clique en dehors de la carte
-                                    showFilterSheet = false
-                                }
-                            
-                            FilterMenuView(selectedFilter: $filterStreetArt, isPresented: $showFilterSheet)
-                            
-                                .padding(.top, 2)
-                                .padding(.trailing, 16)
-                                .transition(.scale.combined(with: .opacity)) // Animation zoom + fondu
+                    }// ZStack
+                    .toolbar{
+                        ToolbarItem(placement: .topBarTrailing) {
+                            Button {
+                                showFilterSheet.toggle() // alterne entre true et false
+                            } label: {
+                                Image(systemName: "line.3.horizontal.decrease.circle")
+                            }
                         }
                     }
-                }// ZStack
-                .toolbar{
-                    ToolbarItem(placement: .topBarTrailing) {
-                        Button {
-                            showFilterSheet.toggle() // alterne entre true et false
-                        } label: {
-                            Image(systemName: "line.3.horizontal.decrease.circle")
-                        }
-                    }
-                }
+                    
+                    // Applique l'animation fluide dès que la var showFilterSheet change
+                    .animation(.easeInOut, value: showFilterSheet)
                 
-                // Applique l'animation fluide dès que la var showFilterSheet change
-                .animation(.easeInOut, value: showFilterSheet)
             }
         }
     }
